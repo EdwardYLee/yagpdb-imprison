32659725eda712a9f259611b80a9a21eb0dad2ea
EdwardYLee <119487283+EdwardYLee@users.noreply.github.com>
2022-12-04 23:15:46 -0800

    Initial Commit

---
+++ Mute.go
@@ -0,0 +1,64 @@
+{{/*
+	Designed for servers with dedicated mute roles and channels. Will store the original roles
+	within a pinned comment on the respecive mute channel and remove all previous roles. 
+	Also adds a random unused mute role. If no unused mute role exists, then this would fail out.
+	There is an optional command that can be passed to force in a particular role regardless of
+	the number of accounts that have that role.
+	
+	Usage:
+		/mute @user. ***NOT YET IMPLEMENTED***
+		/mute @user @muteRole
+		
+	Future Edits:
+		Move muted roles to database with special tag so it can be accessed across commands
+		
+		Add check to see if user is already muted, otherwise the user will have all their old roles
+		erase in the db and reassigned the muted role upon being muted
+*/}}
+
+{{/* Configurable values */}}
+{{ $mutedRoleChannelDict := sdict "1048391685215899768" "1049179984343879741" "1049180079713947720" "1049180201801756692" }}
+{{ $mutedRoles := cslice "1048391685215899768" "1049180079713947720" }}
+{{ $roleToAssign := "" }}
+{{ $continueMuting := 1 }}
+{{ $previousRoles := "" }}
+{{ $member := "" }}
+{{/* End of configurable values */}}
+
+{{if ge (len .CmdArgs) 1}}
+	{{if ge (len .CmdArgs) 2}}
+		{{ $roleToAssign = (getRole (index .CmdArgs 1)) }}
+		
+		{{ if not $roleToAssign }}
+			**Error:** Invalid Role: ``{{(index .CmdArgs 1) }}``
+			{{ $continueMuting = 0 }}
+		{{ end }}
+	{{ end }}
+	
+	{{ if $continueMuting }}
+		{{ $member = (getMember (index .CmdArgs 0)) }}
+
+		{{ if $member }}
+			{{ $previousRoles = $member.Roles }}
+			
+			
+			{{ range $index, $role := $member.Roles }}
+				{{- takeRoleID $member.User.ID $role -}} 
+			{{- end }}
+			
+			{{ dbSet $member.User.ID "previousRoles" $previousRoles }}
+			{{ if $roleToAssign }}
+				{{ giveRoleID $member.User.ID $roleToAssign.ID }}
+			{{ else }}
+				**Error:** Assigning random unused role still under construction
+			{{ end }}
+		{{ else }}
+			**Error:** Invalid Member ``{{ (index .CmdArgs 0) }}``
+			{{ $continueMuting = 0 }}
+		{{ end }}
+	{{ end }}
+{{ end }}
+
+{{ if $continueMuting }}
+	{{ print "<@" $member.User.ID ">" "has been imprisoned in <#" ($mutedRoleChannelDict.Get (print $roleToAssign.ID)) ">" }}
+{{ end }}
---
+++ Unmute.go
@@ -0,0 +1,52 @@
+{{/*
+	Designed for servers with dedicated mute roles and channels.
+	Based on the mute role setting. Will read the pinned comment and remove the muted role and
+	assign back all the previous roles they had. Only works if the user has one of the muted roles.
+	
+	Usage:
+		/unmute @user
+	
+	Future Edits:
+		Move muted roles to database with special tag so it can be accessed across commands
+*/}}
+
+{{/* Configurable values */}}
+{{ $mutedRoleChannelDict := sdict "1048391685215899768" "1049179984343879741" "1049180079713947720" "1049180201801756692" }}
+{{ $mutedRoles := cslice "1048391685215899768" "1049180079713947720" }}
+{{ $foundMutedRole := 0 }}
+{{ $previousRoles := "" }}
+{{ $member := "" }}
+{{/* End of configurable values */}}
+
+{{if ge (len .CmdArgs) 1}}
+	{{ $member = (getMember (index .CmdArgs 0)) }}
+	
+	{{ range $mutedRoles }}
+		{{- if (targetHasRoleID $member.User.ID .) -}}
+			{{- $foundMutedRole = . -}}
+			{{- break }}
+		{{- end -}}
+	{{- end -}}
+		
+	{{ if $foundMutedRole }}
+		{{/* No roles will be saved on unmute*/}}
+		{{ takeRoleID $member.User.ID $foundMutedRole }}
+		
+		{{ $previousRoles =(dbGet $member.User.ID "previousRoles") }}
+		
+		{{ if $previousRoles }}
+			{{ range $previousRoles.Value }}
+				{{- giveRoleID $member.User.ID . -}} 
+			{{- end }}
+		{{ else }}
+			**Error:** No previous roles found for user
+		{{ end }}
+		
+	{{ else }}
+		**Error:** User not currently muted
+	{{ end }}
+{{ end }}
+
+{{ if $foundMutedRole }}
+	{{ print "<@" $member.User.ID ">" "has been releaseed from <#" ($mutedRoleChannelDict.Get $foundMutedRole) ">" }}
+{{ end }}
